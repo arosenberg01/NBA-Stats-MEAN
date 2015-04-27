4a2bf76c4a09b0b031dd3bb66c16d32547cef80c
Ansel Rosenberg <ansel01@gmail.com>
2015-04-27 13:04:23 -0700

    Add tooltip on hover

--- public/app.js
+++ public/app.js
@@ -1,18 +1,15 @@
+function log(input) {
+
+  console.log('working', input)
+};
 
 var nbaApp = angular.module('nbaApp', []);
-
-// var mainController = function($scope, $http) {
 
 nbaApp.controller('mainController', function($scope, $http) {
   $scope.data = {};
 
-  $scope.data = [
-        {display_name: "Greg", rank: 98},
-        {display_name: "Ari", rank: 96},
-        {display_name: 'Q', rank: 75},
-        {display_name: "Loser", rank: 48}
-      ];
-  
+  $scope.test = "blehh"
+
   $scope.items = [
     { category: 'points_per_game', name: 'Points per game' },
     { category: 'assists_per_game', name: 'Assists per game' },
@@ -22,49 +19,50 @@ nbaApp.controller('mainController', function($scope, $http) {
     { category: 'blocks_per_game', name: 'Blocks per game'  },
     { category: 'steals_per_game', name: 'Steals per game'  },
     { category: 'minutes_per_game', name: 'Minutes per game'  },
-    { category: 'assists_to_turnovers_per_game', name: 'Assits to turnovers per game'  },
+    { category: 'assists_to_turnovers_per_game', name: 'Assists to turnovers per game'  },
     { category: 'steals_to_turnovers_per_game', name: 'Steals to turnovers per game'  },
     { category: 'field_goal_pct', name: 'Field goal percentage' },
     { category: 'free_throw_pct', name: 'Free throw percentage'  },
     { category: 'three_point_pct', name: 'Three point percentage'  }
   ];
 
-  $scope.getLeaders = function(category) {
-    console.log($scope.selection.category);
+  $scope.getLeaders = function() {
 
     $http.get('/' + $scope.selection.category)
       .success(function(data) {
           $scope.data = data;
-          console.log(data[0].display_name);
           console.log(data);
           $scope.$apply();
       })
       .error(function(data) {
           console.log('Error: ' + data);
       });
-  }
+  };
+
 });
 
-nbaApp.directive('barGraph', function($timeout) {
+nbaApp.directive('barGraph', function($timeout, $window) {
   var link = function(scope, element, attr) {
+
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
+
+
     var svg = d3.select(element[0])
       .append('svg')
       .style('width', '100%');
 
-      // window.onresize = function() {
-      //   scope.$apply();
-      // };
+      window.onresize = function() {
+        scope.$apply();
+      };
       
       var margin = 20;
       var barHeight = 20;
       var barPadding = 5;
-
-      // scope.data = [
-      //   {name: "Greg", score: 98},
-      //   {name: "Ari", score: 96},
-      //   {name: 'Q', score: 75},
-      //   {name: "Loser", score: 48}
-      // ];
 
       // scope.$watch(function() {
       //     return angular.element($window)[0].innerWidth;
@@ -73,26 +71,20 @@ nbaApp.directive('barGraph', function($timeout) {
       //   });
 
       scope.render = function(data) {
-
-        console.log(data);
      
         svg.selectAll('*').remove();
 
         if (!data) return;
-        // if (renderTimeout) clearTimeout(renderTimeout);
-
-        // renderTimeout = $timeout(function() {
-
 
         $timeout(function() {
-          var width = d3.select(element[0])[0][0].offsetWidth - margin,
-            height = scope.data.length * (barHeight + barPadding),
-            color = d3.scale.category20(),
-            xScale = d3.scale.linear()
-              .domain([0, d3.max(data, function(d) {
-                return d.value;
-              })])
-              .range([0, width]);
+          var width = d3.select(element[0])[0][0].offsetWidth - margin;
+          var height = scope.data.length * (barHeight + barPadding);
+          var color = d3.scale.category20();
+          var xScale = d3.scale.linear()
+            .domain([0, d3.max(data, function(d) {
+              return d.value;
+            })])
+            .range([0, width]);
 
           svg.attr('height', height);
 
@@ -100,9 +92,6 @@ nbaApp.directive('barGraph', function($timeout) {
             .data(data)
             .enter()
               .append('rect')
-              .on('click', function(d,i) {
-                return scope.onClick({item: d});
-              })
               .attr('height', barHeight)
               .attr('width', 140)
               .attr('x', Math.round(margin/2))
@@ -116,7 +105,8 @@ nbaApp.directive('barGraph', function($timeout) {
               .duration(1000)
               .attr('width', function(d) {
                 return xScale(d.value);
-              });
+              })
+              
            svg.selectAll('text')
              .data(data)
              .enter()
@@ -128,17 +118,35 @@ nbaApp.directive('barGraph', function($timeout) {
               .attr('x', 15)
               .text(function(d) {
                 return d.display_name + " (" + d.value + ")";
-              });
+              })
+              .on("mouseover", function(d){return tooltip.style("visibility", "visible")
+                .html('<div>' + d.display_name + '</div>'
+                  + '<div>Rank: ' + d.rank + '</div>'
+                  + '<div>Team: ' + d.team.full_name + '</div>'
+                );})
+              .on("mousemove", function(){return tooltip.style("top",
+                  (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+100)+"px");})
+              .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+              // .on('mouseover', function(d, i) {
+              //   return "log(" + i + ")"
+              // });
+              // .attr("onmouseover", function(d, i) {
+
+              
+
+
+                // return "log(" + i + ")"
+             
+              // .attr("onmouseexit", function(d, i) {
+              //   return "log(" + i + ")"
+              // });
+
         }, 200);
       };
 
-      scope.$watch('data', function(newVals, oldVals) {
-        return scope.render(newVals);
-      }, true);
-
-      // scope.render(scope.data);
-
-
+      scope.$watch('data', function(newData) {
+        return scope.render(newData);
+      }, true);  
   };
     
   return {
--- server.js
+++ server.js
@@ -5,25 +5,20 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
 var db;
-var leadersColl;
 
 MongoClient.connect('mongodb://localhost/nba', function(err, database) {
-  // assert.equal(null, err);
   if (err) console.log(err);
   console.log('\n**************************************');
   console.log("*  Connected correctly to database   *");
   console.log('**************************************\n');
   
   db = database;
-  leadersColl = db.collection('leaders');
 
 });
 
 app.use(express.static(__dirname + '/public'));   
 app.use(morgan('dev'));                                 
-app.use(bodyParser.json());                                     
 
-// listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
 
@@ -34,5 +29,5 @@ app.get('/:category', function(req, res) {
 });
 
 app.get('*', function(req, res) {
-  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  res.sendfile('./public/index.html');
 });
