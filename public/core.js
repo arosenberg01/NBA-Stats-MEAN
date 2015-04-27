cb26387b48e677a3891fed52575cbccc95befbe7
Ansel Rosenberg <ansel01@gmail.com>
2015-04-26 23:00:34 -0700

    Add (hard-coded) d3 bar chart through custom directive

--- public/core.js
+++ public/core.js
@@ -14,10 +14,10 @@ var mainController = function($scope, $http) {
     { category: 'steals_per_game', name: 'Steals per game'  },
     { category: 'minutes_per_game', name: 'Minutes per game'  },
     { category: 'assists_to_turnovers_per_game', name: 'Assits to turnovers per game'  },
+    { category: 'steals_to_turnovers_per_game', name: 'Steals to turnovers per game'  },
     { category: 'field_goal_pct', name: 'Field goal percentage' },
     { category: 'free_throw_pct', name: 'Free throw percentage'  },
-    { category: 'three_point_pct', name: 'Three point percentage'  },
-    { category: 'steals_to_turnovers_per_game', name: 'Steals to turnovers per game'  },
+    { category: 'three_point_pct', name: 'Three point percentage'  }
   ];
 
   $scope.getLeaders = function(category) {
@@ -27,11 +27,110 @@ var mainController = function($scope, $http) {
       .success(function(data) {
           $scope.data = data;
           console.log(data[0].display_name);
+          // console.log(data);
       })
       .error(function(data) {
           console.log('Error: ' + data);
       });
-    
   }
-
 };
+
+nbaApp.directive('helloWorld', function() {
+  var link = function(scope, element, attr) {
+    element.text('Hello World!');
+    var svg = d3.select(element[0])
+      .append('svg')
+      .style('width', '100%');
+
+      // window.onresize = function() {
+      //   scope.$apply();
+      // };
+      
+      var margin = 20;
+      var barHeight = 20;
+      var barPadding = 5;
+      
+      scope.data = [
+        {name: "Greg", score: 98},
+        {name: "Ari", score: 96},
+        {name: 'Q', score: 75},
+        {name: "Loser", score: 48}
+      ];
+
+      // scope.$watch(function() {
+      //     return angular.element($window)[0].innerWidth;
+      //   }, function() {
+      //     scope.render(scope.data);
+      //   });
+
+      scope.render = function(data) {
+     
+        svg.selectAll('*').remove();
+
+        if (!data) return;
+        // if (renderTimeout) clearTimeout(renderTimeout);
+
+        // renderTimeout = $timeout(function() {
+          var width = d3.select(element[0])[0][0].offsetWidth - margin,
+            height = scope.data.length * (barHeight + barPadding),
+            color = d3.scale.category20(),
+            xScale = d3.scale.linear()
+              .domain([0, d3.max(data, function(d) {
+                return d.score;
+              })])
+              .range([0, width]);
+
+        svg.attr('height', height);
+
+        svg.selectAll('rect')
+          .data(data)
+          .enter()
+            .append('rect')
+            .on('click', function(d,i) {
+              return scope.onClick({item: d});
+            })
+            .attr('height', barHeight)
+            .attr('width', 140)
+            .attr('x', Math.round(margin/2))
+            .attr('y', function(d,i) {
+              return i * (barHeight + barPadding);
+            })
+            .attr('fill', function(d) {
+             return color(d.score);
+            })
+            .transition()
+            .duration(1000)
+            .attr('width', function(d) {
+              return xScale(d.score);
+            });
+         svg.selectAll('text')
+           .data(data)
+           .enter()
+            .append('text')
+            .attr('fill', '#fff')
+            .attr('y', function(d,i) {
+              return i * (barHeight + barPadding) + 15;
+            })
+            .attr('x', 15)
+            .text(function(d) {
+              return d.name + " (scored: " + d.score + ")";
+            });
+        // }, 200);
+      };
+
+      scope.render(scope.data);
+
+
+  };
+    
+  return {
+    link: link,
+    scope: {},
+    restrict: 'EA'
+  }
+});
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var app = express();        
-var mongoose = require('mongoose');         
+var app = express();               
 var morgan = require('morgan');      
 var bodyParser = require('body-parser');   
-var methodOverride = require('method-override'); 
-
 var MongoClient = require('mongodb').MongoClient;
-
-// mongoose.connect('mongodb://localhost/nba');     // connect to mongoDB database on modulus.io
 
 var db;
 var leadersColl;
@@ -24,12 +19,9 @@ MongoClient.connect('mongodb://localhost/nba', function(err, database) {
 
 });
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));   
+app.use(morgan('dev'));                                 
+app.use(bodyParser.json());                                     
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
@@ -37,10 +29,8 @@ console.log("App listening on port 8080");
 
 app.get('/:category', function(req, res) {
  db.collection('leaders').find({ 'category': req.params.category }).toArray(function(err, leaders) {
-   // console.log(leaders[0].leaders);
    res.send(leaders[0].leaders)
  });
-
 });
 
 app.get('*', function(req, res) {
