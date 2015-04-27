9b39640fea46902f67b45b66a8e09cfd66b74082
Ansel Rosenberg <ansel01@gmail.com>
2015-04-26 23:38:38 -0700

    Implement dynamic chart creation from database call

--- public/core.js
+++ public/core.js
@@ -1,8 +1,17 @@
 
 var nbaApp = angular.module('nbaApp', []);
 
-var mainController = function($scope, $http) {
+// var mainController = function($scope, $http) {
+
+nbaApp.controller('mainController', function($scope, $http) {
   $scope.data = {};
+
+  $scope.data = [
+        {display_name: "Greg", rank: 98},
+        {display_name: "Ari", rank: 96},
+        {display_name: 'Q', rank: 75},
+        {display_name: "Loser", rank: 48}
+      ];
   
   $scope.items = [
     { category: 'points_per_game', name: 'Points per game' },
@@ -27,17 +36,17 @@ var mainController = function($scope, $http) {
       .success(function(data) {
           $scope.data = data;
           console.log(data[0].display_name);
-          // console.log(data);
+          console.log(data);
+          $scope.$apply();
       })
       .error(function(data) {
           console.log('Error: ' + data);
       });
   }
-};
+});
 
-nbaApp.directive('helloWorld', function() {
+nbaApp.directive('barGraph', function($timeout) {
   var link = function(scope, element, attr) {
-    element.text('Hello World!');
     var svg = d3.select(element[0])
       .append('svg')
       .style('width', '100%');
@@ -49,13 +58,13 @@ nbaApp.directive('helloWorld', function() {
       var margin = 20;
       var barHeight = 20;
       var barPadding = 5;
-      
-      scope.data = [
-        {name: "Greg", score: 98},
-        {name: "Ari", score: 96},
-        {name: 'Q', score: 75},
-        {name: "Loser", score: 48}
-      ];
+
+      // scope.data = [
+      //   {name: "Greg", score: 98},
+      //   {name: "Ari", score: 96},
+      //   {name: 'Q', score: 75},
+      //   {name: "Loser", score: 48}
+      // ];
 
       // scope.$watch(function() {
       //     return angular.element($window)[0].innerWidth;
@@ -64,6 +73,8 @@ nbaApp.directive('helloWorld', function() {
       //   });
 
       scope.render = function(data) {
+
+        console.log(data);
      
         svg.selectAll('*').remove();
 
@@ -71,61 +82,68 @@ nbaApp.directive('helloWorld', function() {
         // if (renderTimeout) clearTimeout(renderTimeout);
 
         // renderTimeout = $timeout(function() {
+
+
+        $timeout(function() {
           var width = d3.select(element[0])[0][0].offsetWidth - margin,
             height = scope.data.length * (barHeight + barPadding),
             color = d3.scale.category20(),
             xScale = d3.scale.linear()
               .domain([0, d3.max(data, function(d) {
-                return d.score;
+                return d.value;
               })])
               .range([0, width]);
 
-        svg.attr('height', height);
+          svg.attr('height', height);
 
-        svg.selectAll('rect')
-          .data(data)
-          .enter()
-            .append('rect')
-            .on('click', function(d,i) {
-              return scope.onClick({item: d});
-            })
-            .attr('height', barHeight)
-            .attr('width', 140)
-            .attr('x', Math.round(margin/2))
-            .attr('y', function(d,i) {
-              return i * (barHeight + barPadding);
-            })
-            .attr('fill', function(d) {
-             return color(d.score);
-            })
-            .transition()
-            .duration(1000)
-            .attr('width', function(d) {
-              return xScale(d.score);
-            });
-         svg.selectAll('text')
-           .data(data)
-           .enter()
-            .append('text')
-            .attr('fill', '#fff')
-            .attr('y', function(d,i) {
-              return i * (barHeight + barPadding) + 15;
-            })
-            .attr('x', 15)
-            .text(function(d) {
-              return d.name + " (scored: " + d.score + ")";
-            });
-        // }, 200);
+          svg.selectAll('rect')
+            .data(data)
+            .enter()
+              .append('rect')
+              .on('click', function(d,i) {
+                return scope.onClick({item: d});
+              })
+              .attr('height', barHeight)
+              .attr('width', 140)
+              .attr('x', Math.round(margin/2))
+              .attr('y', function(d,i) {
+                return i * (barHeight + barPadding);
+              })
+              .attr('fill', function(d) {
+               return color(d.value);
+              })
+              .transition()
+              .duration(1000)
+              .attr('width', function(d) {
+                return xScale(d.value);
+              });
+           svg.selectAll('text')
+             .data(data)
+             .enter()
+              .append('text')
+              .attr('fill', '#fff')
+              .attr('y', function(d,i) {
+                return i * (barHeight + barPadding) + 15;
+              })
+              .attr('x', 15)
+              .text(function(d) {
+                return d.display_name + " (" + d.value + ")";
+              });
+        }, 200);
       };
 
-      scope.render(scope.data);
+      scope.$watch('data', function(newVals, oldVals) {
+        return scope.render(newVals);
+      }, true);
+
+      // scope.render(scope.data);
 
 
   };
     
   return {
     link: link,
-    scope: {},
+    scope: { data: '=' },
     restrict: 'EA'
   }
 });
